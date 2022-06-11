8481f17954a886aa4a096eac2479d4d1e2b64a74
carternadain <cnadain@gmail.com>
2022-06-11 14:10:38 -0700

    deploying

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/PWA-Budget-Tracker";
 const app = express();
+
 
 app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
 app.use(express.static("public"));
+
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
