58f4cd0a0600a39494ad7102bf678d78615f4b80
carternadain <cnadain@gmail.com>
2022-06-11 14:27:04 -0700

    deploying app 2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/PWA-Budget-Tracker";
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/PWA-Budget-Tracker";
 const app = express();
 
 
@@ -16,10 +16,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || '"mongodb://localhost/PWA-Budget-Tracker"', {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 // routes
