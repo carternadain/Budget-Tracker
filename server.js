b0a3eaec49fcb48476e50dee94272ce302d17f6c
carternadain <cnadain@gmail.com>
2022-06-11 14:40:24 -0700

    update deployment

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/PWA-Budget-Tracker";
 const app = express();
 
-
 app.use(logger("dev"));
 
 app.use(compression());
